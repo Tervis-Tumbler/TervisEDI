a85e79975d46a2f7afc7f5d57b725ba072ff2f8b
ChrisMagnuson <hourglasssand@gmail.com>
2017-08-31 10:02:58 -0400

    Initial commit

---
+++ TervisEDI.psm1
@@ -0,0 +1,23 @@
+﻿function Convert-EDIFileCharacterEncodingToASCII {
+    param (
+        [Parameter(Mandatory)]$SourcePath,
+        $DestinationPath
+    )
+    $Files = Get-ChildItem -Path $SourcePath -File
+
+    $FilePathDirectory = if ($DestinationPath) {
+        $DestinationPath
+    } else {
+        "$SourcePath\Converted"
+    }
+
+    New-Item -Path $FilePathDirectory -ItemType Directory -Force | Out-Null
+
+    foreach ($File in $Files) {
+        [xml]$XML = $File | Get-Content        
+
+        $XML.EdiPlaceHolder.Line.content | 
+        Out-File -FilePath $FilePathDirectory\$($File.Name) -NoNewline -Encoding ascii
+        out-
+    }
+}
